9a67ba18f64b40abc3fb0217bbae5048e6daf20e
meghank2795 <125911259+meghank2795@users.noreply.github.com>
2023-03-20 20:58:18 +0530

    changes made

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "ec2-instance" {
+resource "aws_instance" "ec2-instance"{
   ami           =ami-042890a2c52315d75
   instance_type = "t2.micro"
 
   tags = {
     Name = "terraform"
   }
+}
